781ccc9b3b12660dc0d2f877ce8e5c9ef5b269cc
DiscoverSDK <liran@lirans-MacBook-Pro.local>
2016-08-24 16:34:03 +0300

    first working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-var test = 1;
-console.log(test); 
+var mod = function() {
+    var init = function () {
+        var s = document.getElementsByTagName("MyGrid");
+        if (s.length > 0)
+            s[0].outerHTML = "<table border='1'><tr><td>val1</td><td>val2</td></tr><tr><td>val1</td><td>val2</td></tr></table>";
+    }
+
+
+    init();
+}();
